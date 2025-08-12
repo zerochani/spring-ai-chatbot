1884860106dcfb8a1ec8821f42c3eaf73469dd17
youngchanpark <youngchan0510@gmail.com>
2025-08-12 17:17:15 +0900

    feat: 채팅 요청을 위한 DTO 추가

---
+++ src/main/java/dev/myproject/springaichatbot/dto/ChatRequestDto.java
@@ -0,0 +1,15 @@
+package dev.myproject.springaichatbot.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatRequestDto {
+    private String userId;
+    private String message;
+}
