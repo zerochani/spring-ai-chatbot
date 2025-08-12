e686c6da87ea1ed68cbc04a8da80397033e64faf
youngchanpark <youngchan0510@gmail.com>
2025-08-12 17:17:49 +0900

    feat: 채팅 응답을 위한 DTO 추가

---
+++ src/main/java/dev/myproject/springaichatbot/dto/ChatResponseDto.java
@@ -0,0 +1,13 @@
+package dev.myproject.springaichatbot.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ChatResponseDto {
+    private String response;
+}
