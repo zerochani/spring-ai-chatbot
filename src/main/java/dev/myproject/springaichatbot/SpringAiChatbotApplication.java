6a5a201243b5a32d037fe3e4217d256acc095fdb
youngchanpark <youngchan0510@gmail.com>
2025-08-12 17:19:52 +0900

    fix: 수정

--- src/main/java/dev/myproject/springaichatbot/SpringAiChatbotApplication.java
+++ src/main/java/dev/myproject/springaichatbot/SpringAiChatbotApplication.java
@@ -3,11 +3,11 @@ package dev.myproject.springaichatbot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringAiChatbotApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringAiChatbotApplication.class, args);
     }
-
 }
